140ad038f513e88cf2aee077467cf49e164a07cb
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-05-19 15:29:20 +0100

    Fix SVM example

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,14 +21,16 @@ from sklearn.metrics import accuracy_score
 features_train, features_test, labels_train, labels_test = preprocess()
 
 # Training
-clf = svm.SVC()
+clf = svm.SVC(kernel='linear')
 
+# Cut the data by 1/100
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
+# Fit features and labels
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time: ", round(time()-t0, 3), "s"
-
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
 
 # Prediction
 t0 = time()
