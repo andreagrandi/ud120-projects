a123a00ffa241e64a1ff1959cf616338edff7355
Andrea Grandi <andrea.grandi@digital.cabinet-office.gov.uk>
2017-05-19 11:34:02 +0100

    Add SVN exercise

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,29 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# Training
+clf = svm.SVC()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: ", round(time()-t0, 3), "s"
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
-#########################################################
-### your code goes here ###
+# Prediction
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: ", round(time()-t0, 3), "s"
 
-#########################################################
-
-
+# Accuracy
+accuracy = accuracy_score(labels_test, pred)
+print 'Accuracy: {0}'.format(accuracy)
